e0ecfc7fd0fa111d9d8c1626ac5a669e77940fe1
lampert309 <lampert309@gmail.com>
2020-01-06 11:39:40 -0800

    Created the class URContext to make the model binded database URContext stands for Unleashed Rage Context

---
+++ UnleashedRage/Database/URContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnleashedRage.Models;
+
+namespace UnleashedRage.Database
+{
+    public class URContext : DbContext
+    {
+        public URContext(DbContextOptions<URContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ComicPage> ComicPage { get; set; }
+        public DbSet<Merch> Merch { get; set; }
+        public DbSet<User> User { get; set; }
+    }
+}
