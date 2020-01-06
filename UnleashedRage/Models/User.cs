7de23664095ba4acb24e85130bc010842f37139b
lampert309 <lampert309@gmail.com>
2020-01-06 10:52:52 -0800

    Added a new class for the model binded database, User This class will be for users to enter credentials to sign up or log into the service

---
+++ UnleashedRage/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnleashedRage.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserID { get; set; }
+
+        [Required, MaxLength(32), MinLength(3)]
+        public string Username { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required, DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        public string CurrPage { get; set; }
+    }
+}
