23ce900078d312dea7f4daf3648f79423efa7a0c
lampert309 <lampert309@gmail.com>
2020-01-27 11:21:37 -0800

    Changed the hyperlinks to buttons in the archive, and added a test jacascript fuction to test the buttons right now the buttons do nothing, somthing wrong with the onclick or running any javascript functions

--- UnleashedRage/wwwroot/js/site.js
+++ UnleashedRage/wwwroot/js/site.js
@@ -3,3 +3,23 @@
 
 // Write your JavaScript code.
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("Archive").style.width = "250px";
+    // document.getElementById("ArchiveOpen").style.visibility = "hidden"
+}
+
+function closeNav() {
+    document.getElementById("Archive").style.width = "0";
+    // document.getElementById("ArchiveOpen").style.visibility = "visible"
+}
+
+function changeImage(page) {
+    var base64 = Convert.ToBase64String(page);
+    var imgSrc = String.Format("data:image/jpeg;base64,{0}", base64);
+
+    document.getElementById("CurrentPage").src = imgSrc;
+}
+
+function test() {
+    document.getElementById("Tite") = "Test";
+}
