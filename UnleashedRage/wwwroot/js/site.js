67878f61e8517ae2de8c71c2f66ddb17f015ebbe
lampert309 <lampert309@gmail.com>
2020-02-21 12:58:58 -0800

    Changed the javascript code to suposivly make a list of all of the volumes and issues grabed by home, but right now does not do anything

--- UnleashedRage/wwwroot/js/site.js
+++ UnleashedRage/wwwroot/js/site.js
@@ -1,1 +1,9 @@
-﻿
+﻿function openNav() {
+    document.getElementById("Archive").style.width = "50vh";
+    document.getElementById("ArchiveOpen").style.visibility = "hidden"
+}
+
+function closeNav() {
+    document.getElementById("Archive").style.width = "0";
+    document.getElementById("ArchiveOpen").style.visibility = "visible"
+}
