86e4eb9354fae0040026dbe9a58d310e12fc81c6
lampert309 <lampert309@gmail.com>
2020-03-09 09:56:09 -0700

    Changed the functionality to hash the passwords instead of using plain strings to add security

--- UnleashedRage/Database/UserDB.cs
+++ UnleashedRage/Database/UserDB.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using UnleashedRage.Models;
 
@@ -75,9 +76,26 @@ namespace UnleashedRage.Database
         /// </returns>
         public static User AddUser(URContext context, User user)
         {
+            user.Password = HashPassword(user.Password);
             context.Add(user);
             context.SaveChanges();
             return user;
+        }
+
+        /// <summary>
+        /// Hashes the password before it is put into the database for security reasons
+        /// </summary>
+        private static string HashPassword(string password)
+        {
+            byte[] salt;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 10000);
+            byte[] hash = pbkdf2.GetBytes(20);
+            byte[] hashBytes = new byte[36];
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+            string savedPasswordHash = Convert.ToBase64String(hashBytes);
+            return savedPasswordHash;
         }
 
         /// <summary>
@@ -117,10 +135,27 @@ namespace UnleashedRage.Database
         {
             try
             {
-                string password = (from u in context.User
-                                   where u.Username == user.Username
-                                   select u.Password).Single();
-                return password == user.Password;
+                string originalPassword =
+                    (from u in context.User
+                     where u.Username == user.Username
+                     select u.Password).Single();
+
+                /* Fetch the stored value */
+                byte[] hashBytes = Convert.FromBase64String(originalPassword);
+                /* Get the salt */
+                byte[] salt = new byte[16];
+                Array.Copy(hashBytes, 0, salt, 0, 16);
+                /* Compute the hash on the password the user entered */
+                var pbkdf2 = new Rfc2898DeriveBytes(user.Password, salt, 10000);
+                byte[] hash = pbkdf2.GetBytes(20);
+
+                for (int i = 0; i < 20; i++) { 
+                    if (hashBytes[i + 16] != hash[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
             }
             catch
             {
